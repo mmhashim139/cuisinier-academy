c1d8d83ae5ccd9ad22b6e8fa27e5c98efe60c3ec
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-17 11:14:39 +0000

    add overlay navbar and the footer to the base template

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+
+// =========================================================== //
+/*
+Fullscreen Overlay Navbar 
+    Started from W3School https://www.w3schools.com/howto/howto_js_fullscreen_overlay.asp 
+    Customize by add fontawasome bar icon and restyle size & Color
+*/
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+// ============================================================ //

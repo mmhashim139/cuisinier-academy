4eb4dbff13fc5bfae4d1086886508e5255cdd6cf
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-19 15:13:48 +0000

    add initial workshop model for test

--- workshops/models.py
+++ workshops/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=100)
+    sub_title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name

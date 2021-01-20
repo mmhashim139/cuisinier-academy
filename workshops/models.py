105285dcf8c7ed3045881f52f76410688af6921a
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-20 12:54:14 +0000

    add workshop model fields

--- workshops/models.py
+++ workshops/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 
 class Workshop(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    # expert = models.ForeignKey(Expert, on_delete=models.DO_NOTHING)
+    main_img = models.ImageField(null=True, blank=True)
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    image_3 = models.ImageField(null=True, blank=True)
+    agenda_day_1 = models.TextField(null=True, blank=True)
+    agenda_day_2 = models.TextField(null=True, blank=True)
+    agenda_day_3 = models.TextField(null=True, blank=True)
+    price = models.DecimalField(null=True, max_digits=6, decimal_places=2, blank=True)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    location = models.CharField(null=True, max_length=200)
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name

50f77e35067402a5b11a8ae23c1c85ac518f9e13
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-21 16:49:20 +0000

    add workshops related to expert in experts views.py

--- experts/views.py
+++ experts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Expert
+from workshops.models import Workshop
 
 
 def experts(request):
     experts = Expert.objects.all()
+    for expert in experts:
+        workshop = list(Workshop.objects.filter(expert=expert))
+
     context = {
         'experts': experts,
+        'workshop': workshop
     }
 
     return render(request, 'experts/experts.html', context)

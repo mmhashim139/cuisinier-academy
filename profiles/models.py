2ea6c2599c9fd161eb8235bdbb4383b2c7f911dc
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-22 10:39:46 +0000

    create user profile model and render the data into template

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_email = User.username
+    first_name = models.CharField(max_length=15, null=True, blank=True)
+    last_name = models.CharField(max_length=15, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
-        return self.user.email
+        return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import UserProfile
 
 
 def profile(request):
-    return render(request, 'profiles/profile.html')
+    user = UserProfile.objects.all()
+    context = {
+        'user': user,
+    }
+    return render(request, 'profiles/profile.html', context)

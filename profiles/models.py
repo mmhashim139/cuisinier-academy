152390e5f06a5677827ec50368498b433d7294cf
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-24 14:28:43 +0000

    Render order details with user info in reservation page

--- profiles/models.py
+++ profiles/models.py
@@ -5,6 +5,7 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, null=True, blank=True)
     user_email = User.username
     first_name = models.CharField(max_length=15, null=True, blank=True)
     last_name = models.CharField(max_length=15, null=True, blank=True)
@@ -17,4 +18,4 @@ class UserProfile(models.Model):
     country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user_email
--- reservations/views.py
+++ reservations/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from workshops.models import Workshop
 
 
 def create_reservation(request, workshop_id):
-    workshop = get_object_or_404(Workshop, pk=workshop_id)
-    context = {
-        'workshop': workshop,
-    }
+    # if user authinticated , else login / crete an acount
+    if request.user.is_authenticated:
+        workshop = get_object_or_404(Workshop, pk=workshop_id)
+        user = request.user
+        context = {
+            'workshop': workshop,
+            'user': user,
+            }
+        return render(request, 'reservations/reservations.html', context)
 
-    return render(request, 'reservations/reservations.html', context)
+    else:
+        # login and then redirect to the orfer page ???
+        template = "account/login.html"
+        return render(request, template)

1cbb5132302d8c2af4ff5a911613eb61b216d63b
Mohamed Hashim <mmhashim139@gmail.com>
2021-01-28 04:58:39 +0000

    update home app to render experts & workshops model data

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from experts.models import Expert
+from workshops.models import Workshop
 # Create your views here.
 
 
 def index(request):
-    """ A view to return the index page """
+    experts = Expert.objects.all()
+    workshops = Workshop.objects.all()
+    context = {
+        'experts': experts,
+        'workshops': workshops,
+    }
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
